a655bc6ae8bc3907de98bd9df6de51db4a6194ad
Yuyao Wang <49739514+olivia3395@users.noreply.github.com>
2026-04-01 16:00:16 -0400

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from .policy_model import PolicyModel, ReferenceModel, ValueHead, load_tokenizer
-
-__all__ = ["PolicyModel", "ReferenceModel", "ValueHead", "load_tokenizer"]
-
-
-from .dataset import PromptDataset, SyntheticPromptDataset, SYNTHETIC_PROMPTS
-
-__all__ = ["PromptDataset", "SyntheticPromptDataset", "SYNTHETIC_PROMPTS"]
-
